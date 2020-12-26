a909a23f8a6a518d9b556500aca03f12e72c45cd
ttwthomas <ttwthomas@gmail.com>
2020-12-26 12:58:38 -0500

    Create requirements.py

---
+++ requirements.py
@@ -0,0 +1,15 @@
+callofduty.py==1.2.1
+certifi==2020.12.5
+chardet==3.0.4
+elasticsearch==7.10.1
+h11==0.9.0
+h2==3.2.0
+hpack==3.0.0
+hstspreload==2020.11.21
+httpcore==0.9.1
+httpx==0.13.3
+hyperframe==5.2.0
+idna==2.10
+rfc3986==1.4.0
+sniffio==1.2.0
+urllib3==1.26.2
